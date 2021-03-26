734143d32b1eb370acad2df4c2a25a970d0e9344
silenc3502 <gcccompil3r@gmail.com>
2021-03-26 17:56:49 +0900

    Finish JPA Basics

--- board/src/main/java/com/example/board/controller/jpa/JpaBoardController.java
+++ board/src/main/java/com/example/board/controller/jpa/JpaBoardController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -19,14 +21,61 @@ public class JpaBoardController {
 
     @GetMapping("/register")
     public void registerForm (JpaBoard jpaBoard, Model model) throws Exception {
-        log.info("register()");
+        log.info("jpa based get register()");
+    }
+
+    @PostMapping("/register")
+    public String register(JpaBoard jpaBoard, RedirectAttributes rttr) throws Exception {
+        log.info("jpa based post register()");
+
+        service.register(jpaBoard);
+
+        rttr.addFlashAttribute("msg", "JPA 기반의 게시판 등록 성공!");
+
+        return "redirect:/jpaboard/list";
     }
 
     @GetMapping("/list")
     public void list(Model model) throws Exception {
         log.info("list()");
 
-        model.addAttribute("list", service.list());
+        model.addAttribute("lists", service.list());
         // model.addAttribute("list", null);
+    }
+
+    @GetMapping("/read")
+    public void read(Long boardNo, Model model) throws Exception {
+        log.info("read()");
+
+        model.addAttribute(service.read(boardNo));
+    }
+
+    @GetMapping("/modify")
+    public void modifyForm(Long boardNo, Model model) throws Exception {
+        log.info("modifyForm()");
+
+        model.addAttribute(service.read(boardNo));
+    }
+
+    @PostMapping("/modify")
+    public String modify(JpaBoard jpaBoard, RedirectAttributes rttr) throws Exception {
+        log.info("modify()");
+
+        service.modify(jpaBoard);
+
+        rttr.addFlashAttribute("msg",
+                "jpa 기반에서 수정이 성공적으로 완료되었습니다.");
+
+        return "redirect:/jpaboard/list";
+    }
+
+    @PostMapping("/remove")
+    public String remove(Long boardNo, RedirectAttributes rttr) throws Exception {
+        service.remove(boardNo);
+
+        rttr.addFlashAttribute("msg",
+                "jpa 기반에서 삭제가 성공적으로 완료되었습니다.");
+
+        return "redirect:/jpaboard/list";
     }
 }
--- board/src/main/java/com/example/board/service/jpa/JpaBoardServiceImpl.java
+++ board/src/main/java/com/example/board/service/jpa/JpaBoardServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.board.service.jpa;
 import com.example.board.entity.jpa.JpaBoard;
 import com.example.board.repository.jpa.JpaBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,26 +18,35 @@ public class JpaBoardServiceImpl implements JpaBoardService {
     @Override
     @Transactional(readOnly = true)
     public List<JpaBoard> list() throws Exception {
-        return null;
+        // findAll이 리스트 전체 찾기
+        // 정렬을 하시오 Descent, 그 대상은 boardNo에 해당한다.
+        return repository.findAll(Sort.by(Sort.Direction.DESC, "boardNo"));
     }
 
     @Override
+    @Transactional
     public void register(JpaBoard jpaBoard) throws Exception {
-
+        repository.save(jpaBoard);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public JpaBoard read(Long boardNo) throws Exception {
-        return null;
+        return repository.getOne(boardNo);
     }
 
     @Override
+    @Transactional
     public void modify(JpaBoard jpaBoard) throws Exception {
+        JpaBoard jpaBoardEntity = repository.getOne(jpaBoard.getBoardNo());
 
+        jpaBoardEntity.setTitle(jpaBoard.getTitle());
+        jpaBoardEntity.setContent(jpaBoard.getContent());
     }
 
     @Override
+    @Transactional
     public void remove(Long boardNo) throws Exception {
-
+        repository.deleteById(boardNo);
     }
 }
